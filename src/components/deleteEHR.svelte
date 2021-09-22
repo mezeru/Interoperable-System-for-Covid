afd2efaf0e3a731fb0df53940483e8ad676b0127
Mezeru <jermpro0@gmail.com>
2021-09-22 08:31:33 +0530

    name updated

--- src/components/deleteEHR.svelte
+++ src/components/deleteEHR.svelte
@@ -29,13 +29,13 @@
       <label
         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
         for="ahrno"
-        name="ahrno"
       >
         Adhaar ID of the Patient
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         placeholder="XXXX XXXX XXXX"
+        name="ahrno"
       />
     </div>
   </form>
