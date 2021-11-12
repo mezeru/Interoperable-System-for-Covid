25d1c82c3346c1a5e66b476895cce61f69e52e32
Mezeru <jermpro0@gmail.com>
2021-11-12 23:07:47 +0530

    Added Native Template

--- src/components/GetOne.svelte
+++ src/components/GetOne.svelte
@@ -63,6 +63,7 @@
           bind:value={number}
           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           placeholder="XXXX XXXX XXXX"
+          type="number"
           name="ahrno"
         />
       </div>
